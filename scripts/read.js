b6e74750025db0b2a3c89147ca195e1afaf8ede6
Ashish Khare <akstyles211@gmail.com>
2024-05-26 11:52:06 +0530

    feat: multiplexed dev with read

--- scripts/read.js
+++ scripts/read.js
@@ -1,13 +1,13 @@
 import { readdir, writeFile } from 'node:fs/promises';
 
 try {
-  const files = await readdir("../public");
+  const files = await readdir("./public");
   let paths = [];
   for (const file of files)
   	if(file.endsWith(".mp4"))
     	paths.push(`/${file}?url`);
   console.log(`${paths.length} videos found.`);
-  const promise = await writeFile('paths.json', JSON.stringify(paths));
+  const promise = await writeFile('./scripts/paths.json', JSON.stringify(paths));
 
 } catch (err) {
   console.error(err);
